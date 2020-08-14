5d97647c77f6be68c0ad8bc82da504eb0c5d9dab
Jay Chen <walkccray@gmail.com>
2020-08-14 16:15:02 +0800

    Inject CustomerDAO into CustomerController

--- src/main/java/com/jaychen/springdemo/controller/CustomerController.java
+++ src/main/java/com/jaychen/springdemo/controller/CustomerController.java
@@ -1,16 +1,29 @@
 package com.jaychen.springdemo.controller;
 
+import com.jaychen.springdemo.dao.CustomerDAO;
 
+import com.jaychen.springdemo.entity.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+  private CustomerDAO customerDAO;
+
   @RequestMapping("/list")
   public String listCustomers(Model model) {
+
+    // get customers from the dao
+    List<Customer> customers = customerDAO.getCustomers();
+
+    // add the customers to the model
+    model.addAttribute("customers", customers);
+
     return "list-customers";
   }
 }
